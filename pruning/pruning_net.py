96af52f5428979b11d7f3850bf4b3bd14c5fe49b
“gCSH's Computer <595470658@qq.com>
2020-04-15 12:20:13 +0800

    使用pytorch提供的prune接口进行剪枝

--- pruning/pruning_net.py
+++ pruning/pruning_net.py
@@ -7,19 +7,91 @@
 import torch
 from pruning.nets import MyNet
 from pruning.utils import weight_prune, filter_prune
+import torch.nn.utils.prune as prune
 
 
 class Pruning:
     def __init__(self, net_path):
         self.net = MyNet()
         self.net.load_state_dict(torch.load(net_path))
+        self.parameters_to_prune = (
+            (self.net.conv1, 'weight'),
+            (self.net.conv2, 'weight'),
+            (self.net.conv3, 'weight'),
+            (self.net.linear1, 'weight'),
+            (self.net.linear2, 'weight'),
+        )
 
     def pruning(self):
-        mask = weight_prune(self.net, 60)
-        self.net.set_masks(mask, True)
-        torch.save(self.net.state_dict(), "models/pruned_net_without_conv.pth")
-        filter_prune(self.net, 50)
-        torch.save(self.net.state_dict(), "models/pruned_net_with_conv.pth")
+        for i in range(1, 10):
+            amount = i * 0.1
+            prune.global_unstructured(
+                self.parameters_to_prune,
+                pruning_method=prune.L1Unstructured,
+                amount=amount,
+            )
+            print(self.net.state_dict().keys())
+            # 删除weight_orig 、weight_mask以及forward_pre_hook
+            prune.remove(self.net.conv1, 'weight')
+            prune.remove(self.net.conv2, 'weight')
+            prune.remove(self.net.conv3, 'weight')
+            prune.remove(self.net.linear1, 'weight')
+            prune.remove(self.net.linear2, 'weight')
+            # print(self.net.linear1.weight)
+            # mask = weight_prune(self.net, 60)
+            # self.net.set_masks(mask, True)
+            # torch.save(self.net.state_dict(), "self.nets/pruned_net_without_conv.pth")
+            # filter_prune(self.net, 50)
+            print(
+                "Sparsity in conv1.weight: {:.2f}%".format(
+                    100. * float(torch.sum(self.net.conv1.weight == 0))
+                    / float(self.net.conv1.weight.nelement())
+                )
+            )
+            print(
+                "Sparsity in conv2.weight: {:.2f}%".format(
+                    100. * float(torch.sum(self.net.conv2.weight == 0))
+                    / float(self.net.conv2.weight.nelement())
+                )
+            )
+            print(
+                "Sparsity in conv3.weight: {:.2f}%".format(
+                    100. * float(torch.sum(self.net.conv3.weight == 0))
+                    / float(self.net.conv3.weight.nelement())
+                )
+            )
+            print(
+                "Sparsity in linear1.weight: {:.2f}%".format(
+                    100. * float(torch.sum(self.net.linear1.weight == 0))
+                    / float(self.net.linear1.weight.nelement())
+                )
+            )
+            print(
+                "Sparsity in linear2.weight: {:.2f}%".format(
+                    100. * float(torch.sum(self.net.linear2.weight == 0))
+                    / float(self.net.linear2.weight.nelement())
+                )
+            )
+            print(
+                "Global sparsity: {:.2f}%".format(
+                    100. * float(
+                        torch.sum(self.net.conv1.weight == 0)
+                        + torch.sum(self.net.conv2.weight == 0)
+                        + torch.sum(self.net.conv3.weight == 0)
+                        + torch.sum(self.net.linear1.weight == 0)
+                        + torch.sum(self.net.linear2.weight == 0)
+                    )
+                    / float(
+                        self.net.conv1.weight.nelement()
+                        + self.net.conv2.weight.nelement()
+                        + self.net.conv3.weight.nelement()
+                        + self.net.linear1.weight.nelement()
+                        + self.net.linear2.weight.nelement()
+                    )
+                )
+            )
+            # torch.save(self.net.state_dict(), "models/pruned_net_with_conv.pth")
+            torch.save(self.net.state_dict(), f"models/pruned_net_with_torch_{amount:.1f}.pth")
 
 
 if __name__ == '__main__':
---
+++ test.py
@@ -0,0 +1,192 @@
+# @Time : 2020-04-14 11:03 
+# @Author : Ben 
+# @Version：V 0.1
+# @File : test.py
+# @desc :
+
+
+import torch
+from torch import nn
+import torch.nn.utils.prune as prune
+import torch.nn.functional as F
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+class LeNet(nn.Module):
+    def __init__(self):
+        super(LeNet, self).__init__()
+        # 1 input image channel, 6 output channels, 3x3 square conv kernel
+        self.conv1 = nn.Conv2d(1, 6, 3)
+        self.conv2 = nn.Conv2d(6, 16, 3)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)  # 5x5 image dimension
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = F.max_pool2d(F.relu(self.conv1(x)), (2, 2))
+        x = F.max_pool2d(F.relu(self.conv2(x)), 2)
+        x = x.view(-1, int(x.nelement() / x.shape[0]))
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+
+model = LeNet().to(device=device)
+
+module = model.conv1
+print(list(module.named_parameters()))
+print(list(module.named_buffers()))
+
+prune.random_unstructured(module, name="weight", amount=0.3)
+# print(list(module.named_parameters()))
+# print(list(module.named_buffers()))
+print(module.weight)
+# print(module._forward_pre_hooks)
+
+# rune the 3 smallest entries in the bias by L1 norm
+prune.l1_unstructured(module, name="bias", amount=3)
+# print(list(module.named_parameters()))
+# print(list(module.named_buffers()))
+print(module.bias)
+# print(module._forward_pre_hooks)
+
+prune.ln_structured(module, name="weight", amount=0.5, n=2, dim=0)
+
+# As we can verify, this will zero out all the connections corresponding to
+# 50% (3 out of 6) of the channels, while preserving the action of the
+# previous mask.
+print(module.weight)
+for hook in module._forward_pre_hooks.values():
+    if hook._tensor_name == "weight":  # select out the correct hook
+        break
+
+print(list(hook))  # pruning history in the container
+
+print(model.state_dict().keys())
+
+print(list(module.named_parameters()))
+prune.remove(module, 'weight')
+prune.remove(module, 'bias')
+print(list(module.named_parameters()))
+
+new_model = LeNet()
+for name, module in new_model.named_modules():
+    # prune 20% of connections in all 2D-conv layers
+    if isinstance(module, torch.nn.Conv2d):
+        prune.l1_unstructured(module, name='weight', amount=0.2)
+    # prune 40% of connections in all linear layers
+    elif isinstance(module, torch.nn.Linear):
+        prune.l1_unstructured(module, name='weight', amount=0.4)
+
+print(dict(new_model.named_buffers()).keys())  # to verify that all masks exist
+
+model = LeNet()
+
+parameters_to_prune = (
+    (model.conv1, 'weight'),
+    (model.conv2, 'weight'),
+    (model.fc1, 'weight'),
+    (model.fc2, 'weight'),
+    (model.fc3, 'weight'),
+)
+
+prune.global_unstructured(
+    parameters_to_prune,
+    pruning_method=prune.L1Unstructured,
+    amount=0.2,
+)
+
+print(
+    "Sparsity in conv1.weight: {:.2f}%".format(
+        100. * float(torch.sum(model.conv1.weight == 0))
+        / float(model.conv1.weight.nelement())
+    )
+)
+print(
+    "Sparsity in conv2.weight: {:.2f}%".format(
+        100. * float(torch.sum(model.conv2.weight == 0))
+        / float(model.conv2.weight.nelement())
+    )
+)
+print(
+    "Sparsity in fc1.weight: {:.2f}%".format(
+        100. * float(torch.sum(model.fc1.weight == 0))
+        / float(model.fc1.weight.nelement())
+    )
+)
+print(
+    "Sparsity in fc2.weight: {:.2f}%".format(
+        100. * float(torch.sum(model.fc2.weight == 0))
+        / float(model.fc2.weight.nelement())
+    )
+)
+print(
+    "Sparsity in fc3.weight: {:.2f}%".format(
+        100. * float(torch.sum(model.fc3.weight == 0))
+        / float(model.fc3.weight.nelement())
+    )
+)
+print(
+    "Global sparsity: {:.2f}%".format(
+        100. * float(
+            torch.sum(model.conv1.weight == 0)
+            + torch.sum(model.conv2.weight == 0)
+            + torch.sum(model.fc1.weight == 0)
+            + torch.sum(model.fc2.weight == 0)
+            + torch.sum(model.fc3.weight == 0)
+        )
+        / float(
+            model.conv1.weight.nelement()
+            + model.conv2.weight.nelement()
+            + model.fc1.weight.nelement()
+            + model.fc2.weight.nelement()
+            + model.fc3.weight.nelement()
+        )
+    )
+)
+
+
+class FooBarPruningMethod(prune.BasePruningMethod):
+    """Prune every other entry in a tensor
+    """
+    PRUNING_TYPE = 'unstructured'
+
+    def compute_mask(self, t, default_mask):
+        mask = default_mask.clone()
+        mask.view(-1)[::2] = 0
+        return mask
+
+def foobar_unstructured(module, name):
+    """Prunes tensor corresponding to parameter called `name` in `module`
+    by removing every other entry in the tensors.
+    Modifies module in place (and also return the modified module)
+    by:
+    1) adding a named buffer called `name+'_mask'` corresponding to the
+    binary mask applied to the parameter `name` by the pruning method.
+    The parameter `name` is replaced by its pruned version, while the
+    original (unpruned) parameter is stored in a new parameter named
+    `name+'_orig'`.
+
+    Args:
+        module (nn.Module): module containing the tensor to prune
+        name (string): parameter name within `module` on which pruning
+                will act.
+
+    Returns:
+        module (nn.Module): modified (i.e. pruned) version of the input
+            module
+
+    Examples:
+        >>> m = nn.Linear(3, 4)
+        >>> foobar_unstructured(m, name='bias')
+    """
+    FooBarPruningMethod.apply(module, name)
+    return module
+
+
+model = LeNet()
+foobar_unstructured(model.fc3, name='bias')
+
+print(model.fc3.bias_mask)
