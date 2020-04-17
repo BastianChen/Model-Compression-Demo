730032bd0068354e4815d6fb104fdaf0281a7f18
“gCSH's Computer <595470658@qq.com>
2020-04-17 21:27:03 +0800

    修改网络结构

--- quantization/nets.py
+++ quantization/nets.py
@@ -9,39 +9,66 @@ from torch import nn
 from torch.quantization import QuantStub, DeQuantStub
 
 
+class Conv(nn.Sequential):
+    def __init__(self, input_channels, output_channels, kernel_size, stride=1, padding=0, groups=1, bias=False):
+        super().__init__(
+            nn.Conv2d(input_channels, output_channels, kernel_size, stride, padding, groups=groups, bias=bias),
+            nn.BatchNorm2d(output_channels),
+            nn.ReLU()
+        )
+
+
+class ResBlock(nn.Module):
+    def __init__(self, input_channels):
+        super().__init__()
+        self.layer = nn.Sequential(
+            Conv(input_channels, input_channels // 2, 1),
+            Conv(input_channels // 2, input_channels // 2, 3, 1, 1),
+            Conv(input_channels // 2, input_channels, 1)
+        )
+        self.skip_add = nn.quantized.FloatFunctional()
+
+    def forward(self, data):
+        return self.skip_add.add(data, self.layer(data))
+
+
 class MyNet(nn.Module):
     def __init__(self):
         super().__init__()
-        self.conv1 = nn.Conv2d(1, 32, kernel_size=3, padding=1, stride=1)
-        self.bn1 = nn.BatchNorm2d(32)
-        self.relu1 = nn.ReLU(inplace=True)
-        self.maxpool1 = nn.MaxPool2d(2)
-
-        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, padding=1, stride=1)
-        self.bn2 = nn.BatchNorm2d(64)
-        self.relu2 = nn.ReLU(inplace=True)
-        self.maxpool2 = nn.MaxPool2d(2)
-
-        self.conv3 = nn.Conv2d(64, 64, kernel_size=3, padding=1, stride=1)
-        self.bn3 = nn.BatchNorm2d(64)
-        self.relu3 = nn.ReLU(inplace=True)
+        self.layer = nn.Sequential(
+            Conv(1, 32, 3, 1, 1),
+            ResBlock(32),
+            nn.MaxPool2d(2),
+            Conv(32, 64, 3, 1, 1),
+            ResBlock(64),
+            ResBlock(64),
+            nn.MaxPool2d(2),
+            Conv(64, 64, 3, 1, 1),
+        )
 
         self.linear1 = nn.Linear(7 * 7 * 64, 128)
         self.relu4 = nn.ReLU(inplace=True)
         self.linear2 = nn.Linear(128, 10)
         self.quant = QuantStub()
         self.dequant = DeQuantStub()
-        self.skip_add = nn.quantized.FloatFunctional()
         self.loss = nn.CrossEntropyLoss()
+
+        # weight initialization
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out')
+                if m.bias is not None:
+                    nn.init.zeros_(m.bias)
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.ones_(m.weight)
+                nn.init.zeros_(m.bias)
+            elif isinstance(m, nn.Linear):
+                nn.init.normal_(m.weight, 0, 0.01)
+                nn.init.zeros_(m.bias)
 
     def forward(self, data):
         data = self.quant(data)
-        out = self.relu1(self.bn1(self.conv1(data)))
-        # data跟out的shape必须一致
-        # out = self.skip_add.add(data, out)
-        out = self.maxpool1(out)
-        out = self.maxpool2(self.relu2(self.bn2(self.conv2(out))))
-        out = self.relu3(self.bn3(self.conv3(out)))
+        out = self.layer(data)
         out = out.reshape(out.size(0), -1)
         out = self.relu4(self.linear1(out))
         out = self.linear2(out)
@@ -52,6 +79,12 @@ class MyNet(nn.Module):
         return self.loss(output, label)
 
     def fuse_model(self):
-        torch.quantization.fuse_modules(self, [['conv1', 'bn1', 'relu1'],
-                                               ['conv2', 'bn2', 'relu2'],
-                                               ['conv3', 'bn3', 'relu3'], ['linear1', 'relu4']], inplace=True)
+        for m in self.modules():
+            if type(m) == Conv:
+                torch.quantization.fuse_modules(m, ['0', '1', '2'], inplace=True)
+        torch.quantization.fuse_modules(self, ['linear1', 'relu4'], inplace=True)
+
+
+if __name__ == '__main__':
+    net = MyNet()
+    net.fuse_model()
